1dcc6d37760d96b94f76b373476e8f6426d0350b
kimolivia0721 <kimolivia0721@gmail.com>
2020-02-03 22:30:01 -0500

    Add files via upload
    uploaded

---
+++ src/com/okvr/game/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package com.okvr.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.okvr.game.Bubble;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 600;
+		config.height = 600;
+		new LwjglApplication(new Bubble(), config);
+	}
+}
